388e47cdcddde30af5cb84a5161d3889012b111c
Hamza-ai-student <56657346+Hamza-ai-student@users.noreply.github.com>
2019-10-29 03:42:26 -0700

    Add files via upload

---
+++ Untitled6.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # HOME WORK 27 OCT 2019 PIAIC BATCH 3 Q 1 ISLAMABAD 
+
+# ## ABS FUNCTION USED TO GET ABSOLUTE VALUE
+
+# In[2]:
+
+
+abs(-989)
+
+
+# In[6]:
+
+
+abs(-89)
+
+
+# In[7]:
+
+
+abs(-678656)
+
+
+# In[8]:
+
+
+abs(-98)
+
+
+# In[10]:
+
+
+abs(765)
+
+
+# # BIN FUNCTION WHICH CAN CONVERT AN INTEGER TO A BINARY STRING
+
+# In[13]:
+
+
+bin(990)
+
+
+# In[15]:
+
+
+bin(90)
+
+
+# In[16]:
+
+
+bin(785543856)
+
+
+# In[18]:
+
+
+bin(76575)
+
+
+# # EVAL FUNCTION WHICH CAN EVALUTES THE SPECIFIC EXPRESSION
+
+# In[22]:
+
+
+x =(1)
+eval('x*3')
+
+
+# In[24]:
+
+
+x =7
+f =8
+d =12
+eval("x*f-d*9")
+
+
+# In[32]:
+
+
+a =100
+b =5
+c =3
+x =50
+d =a+x+b+c
+eval("d-6")
+
+
+# # oct function which can convert the an integer to an octal string 
+
+# In[34]:
+
+
+oct(789)
+
+
+# In[35]:
+
+
+oct(79765)
+
+
+# In[36]:
+
+
+oct(0)
+
+
+# In[43]:
+
+
+oct(1)
+
+
+# In[44]:
+
+
+x = 45
+b = 56
+oct(x+b)
+
+
+# ##  chr function return the string representing  something whose code is integer
+
+# In[45]:
+
+
+chr(7)
+
+
+# In[46]:
+
+
+chr(786)
+
+
+# In[47]:
+
+
+chr(89)
+
+
+# In[48]:
+
+
+chr(69)
+
+
+# In[49]:
+
+
+x = 56
+b = 908
+c = 0
+chr(x*b*c)
+
+
+# ### HAMZA SHARIF
+#      BATCH 3 QUARTER 1
+#      ISLAMABAD AI CLASS
+#      HOMEWORK 
+#      (27 OCT 2019)
